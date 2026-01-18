58cc628cb1f2a0f52921bab80721a5dd327028ed
codersrabbi <pcrabbi2020@gmail.com>
2026-01-18 17:37:58 +0600

    footer section stated but not complete

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../shared/Navbar';
+import Footer from '../shared/Footer';
 
 const Root = () => {
     return (
         <div>
             <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
---
+++ src/shared/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../assets/logo/logo.webp'
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+                <div className='flex gap-2 items-center'>
+                    <img className='w-20' src={logo} alt="" />
+                    <p className='text-2xl'>MN_Blog</p>
+                </div>
+                <p className='text-gray-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br /> Rerum unde quaerat eveniet cumque accusamus atque qui <br /> error quo enim fugiat?</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
